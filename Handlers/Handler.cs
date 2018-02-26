293b201b2b0899d813a574fa005dc616fd769403
Justin Donohoo <jdonohoo@gmail.com>
2018-02-26 12:42:17 -0700

    Bootstrap

---
+++ Handlers/Handler.cs
@@ -0,0 +1,36 @@
+﻿using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.Core;
+using Handlers.Helpers;
+using Handlers.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Handlers
+{
+    public class Handler
+    {
+        public Response Hello(Request request, ILambdaContext context)
+        {
+            context.Logger.LogLine($"{context.FunctionName} execution started");
+            context.Logger.LogLine($"TestString: {AppConfig.Instance.Parameters["TestString"]}");
+            context.Logger.LogLine($"TestSecure: {AppConfig.Instance.Parameters["TestSecure"]}");
+
+            return new Response { Message = "Hello World, serverless-aws-aspnetcore2!" };
+        }
+
+        public APIGatewayProxyResponse HealthCheck(APIGatewayProxyRequest request, ILambdaContext context)
+        {
+            context.Logger.LogLine($"{context.FunctionName} execution started");
+
+            return new APIGatewayProxyResponse()
+            {
+                StatusCode = 200,
+                Headers = new Dictionary<string, string>() { { "Context-Type", "text/html" } },
+                Body = "OK"
+            };
+        }
+
+    }
+}
---
+++ Tests/HandlerTests.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Extensions.Configuration;
+using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.TestUtilities;
+using Handlers;
+using Handlers.Helpers;
+using Handlers.Models;
+using System;
+using Xunit;
+
+namespace Tests
+{
+    public class HandlerTests
+    {
+        [Fact]
+        public void TestHealthCheck()
+        {
+            var request = new APIGatewayProxyRequest();
+            var context = new TestLambdaContext();
+
+            var handler = new Handler();
+            var response = handler.HealthCheck(request, context);
+
+            Assert.Equal(200, response.StatusCode);
+            Assert.Equal("OK", response.Body);
+        }
+
+        [Fact]
+        public void TestHello()
+        {
+            var context = new TestLambdaContext();
+            var handler = new Handler();
+
+            var response = handler.Hello(new Request(), context);
+
+            Assert.Equal("Hello World, serverless-aws-aspnetcore2!",response.Message);
+        }
+                
+        [Theory]
+        [InlineData("dev")]
+        public void ConfigurationTest(string stage)
+        {
+            Environment.SetEnvironmentVariable("region", "us-east-1");
+            Environment.SetEnvironmentVariable("serviceName", "serverless-aws-aspnetcore2");
+            Environment.SetEnvironmentVariable("parameterPath", $"/{stage}/serverless-aws-aspnetcore2/settings/");
+            Environment.SetEnvironmentVariable("ASPNETCORE_ENVIRONMENT", "Development");
+
+            var context = new TestLambdaContext();
+            var handler = new Handler();
+
+            Assert.Equal("serverless-aws-aspnetcore2", AppConfig.Instance.ServiceName);
+            Assert.Equal($"/{stage}/serverless-aws-aspnetcore2/settings/", AppConfig.Instance.ParameterPath);
+            Assert.Equal("Secure string test value", AppConfig.Instance.Parameters["TestSecure"]);
+            Assert.Equal("Some Test String", AppConfig.Instance.Parameters["TestString"]);
+        }
+    }
+}
